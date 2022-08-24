b3c49e01de013b2179c289cfe69b0d3c250b926f
Samir Rajguru <samir@supportlogic.io>
2022-08-24 14:14:22 -0400

    test function

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -2,6 +2,6 @@ exports.handler = async () => {
   const mySecret = process.env.abc;
   return {
     statusCode: 200,
-    body: `hello world! I have a ${mySecret}`,
+    body: `Andrew: hello world! I have a secret ${mySecret}`,
   };
 };
